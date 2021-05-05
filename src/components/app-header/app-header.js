dff0c05b177d12abd134558bcd74824119e4489e
Igor Nazarenko <nazarenko.epson@gmail.com>
2021-05-05 20:41:21 +0600

    first day

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader=()=>{
+    return (
+        <div className="app-header d-flex">
+            <h1>Igor Nazarenko</h1>
+            <h2>5 записей, понравившихся 0</h2>
+        </div>
+    )
+}
+export default AppHeader;
